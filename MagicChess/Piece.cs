9604b0ee99de6c63b553b57f570ac38be539eacc
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2024-03-05 11:22:59 +0700

    new

--- MagicChess/Piece.cs
+++ MagicChess/Piece.cs
@@ -1,11 +1,24 @@
 namespace MagicChess;
 
-public class Piece : IPiece
+public class Piece
 {
 }
 
 public interface IPiece{
-
+    int ID {get;}
+    string name {get;}
+    PieceType charType {get;}
+    Races Races {get;}
+    PieceCategories categories {get;}
+    int Price {get;}
+    // levelsType enum belum dibuat
+    bool isAlive {get;}
+    int HP {get;}
+    int Armor {get;}
+    int AttackPoint {get;}
+    int AttackSpeed {get;}
+    // MagRes?
+    // position
 }
 
 public enum PieceType{
@@ -14,6 +27,14 @@ public enum PieceType{
     Rare,
     Epic,
     Legendary
+}
+
+public enum Races{
+    Pandaman,
+    Civet,
+    Human,
+    Feathered,
+    Demon
 }
 public enum SkillsType{
     BatteryAssault,
