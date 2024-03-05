eecbd0871dbee63005450f586db55df90ba069cb
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2024-03-05 10:10:35 +0700

    new commit

--- MagicChess/Piece.cs
+++ MagicChess/Piece.cs
@@ -7,3 +7,23 @@ public class Piece : IPiece
 public interface IPiece{
 
 }
+
+public enum PieceType{
+    Common,
+    Uncommon,
+    Rare,
+    Epic,
+    Legendary
+}
+public enum SkillsType{
+    BatteryAssault,
+    ParalysisShuriken,
+
+}
+public enum PieceCategories{
+    Knight,
+    Warlock,
+    Mage,
+    Warrior,
+    Hunter
+}
--- MagicChess/Player.cs
+++ MagicChess/Player.cs
@@ -2,6 +2,22 @@ namespace MagicChess;
 
 public class Player : IPlayer
 {
+    public int ID {get;}
+    public string Name {get;}
+    public int GameLevel {get; private set;}
+    public int GameExp {get; private set;}
+
+    public Player(int id, string name){
+        ID = id;
+        Name = name;
+        GameLevel = 0;
+        GameExp = 0;
+    }
 }
 
-public interface IPlayer
+public interface IPlayer{
+    int ID {get;}
+    string Name {get;}
+    int GameLevel {get;}
+    int GameExp {get;}
+}
