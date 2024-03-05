efa03c1e5f4e898c6210780ea16cbbbdceaf1f45
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2024-03-05 13:11:55 +0700

    c

--- MagicChess/BattleArena.cs
+++ MagicChess/BattleArena.cs
@@ -4,9 +4,11 @@ namespace MagicChess;
 
 public class BattleArena : IBattleArena
 {
-
+    public int ID {get; private set;}
+    public string Name {get; private set;}
 }
 
 public interface IBattleArena{
-
+    int ID {get;}
+    string Name {get;}
 }
--- MagicChess/GameController.cs
+++ MagicChess/GameController.cs
@@ -3,3 +3,25 @@ namespace MagicChess;
 public class GameController
 {
 }
+
+public class IAutoChessGameController{
+    int BattleRound {get;}
+    // int BattleTime {get;}
+    int PreparationTime{get;}
+    Dictionary<IPlayer, PlayerData> PlayersData;
+    //List<IPiece> AllPieceList;
+    // List<IPiece> PlayersList;
+    // List<IPiece> ArenaTypeList;
+
+    // kayaknya bakalan bikin 1 kelas buat handle semua pergerakan Ipiece
+    // PieceLocation LocationMoveTo;
+    // List<IPlayer> OpponentList
+    // List<IPlayer> RoundWinnerList
+    // List<IPlayer> RoundLoserList
+    // Action<IPlayer, IPiece, IPiece> PieceAttackDefend
+
+}
+
+public class PlayerData{
+
+}
